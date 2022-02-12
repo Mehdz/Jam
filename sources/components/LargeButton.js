3c12efa5624c14fa53e567d460acac6af75ffccf
Mehdi <mehdi.zehri@epitech.eu>
2022-02-12 18:56:47 +0100

    ✨ Add new parameters to the component.

---
+++ sources/components/LargeButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from 'react-native-paper';
+import PropTypes from 'prop-types';
+
+function LargeButton({children, onPress, mode, icon, color, dark}) {
+  return (
+    <Button
+      contentStyle={styles.btn}
+      style={{marginTop: 20}}
+      mode={mode}
+      icon={icon}
+      color={color}
+      dark={dark}
+      onPress={onPress}>
+      {children}
+    </Button>
+  );
+}
+
+const styles = StyleSheet.create({
+  btn : {
+    padding: 10,
+  }
+});
+
+LargeButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  onPress: PropTypes.any,
+  mode: PropTypes.string.isRequired,
+  icon : PropTypes.string,
+  color: PropTypes.string,
+  dark: PropTypes.bool,
+};
+
+export default LargeButton;
