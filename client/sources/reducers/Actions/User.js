7d427c704b91e097dfb241a6c4f42e7bba92d0b4
Mehdi <mehdi.zehri@epitech.eu>
2022-02-13 02:38:42 +0100

    🎨 Imporve the user reducer.

--- client/sources/reducers/Actions/User.js
+++ client/sources/reducers/Actions/User.js
@@ -1,11 +1,27 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+export const SET_USER_QUESTION = 'SET_USER_QUESTION';
 export const SET_USER_LEVEL = 'SET_USER_LEVEL';
 export const SET_USER_DIFFCULTY = 'SET_USER_DIFFCULTY';
 export const SET_USER_SCORE = 'SET_USER_SCORE';
 export const SET_USER_DATA = 'SET_USER_DATA';
 
-export const setUserLevel = (level) => dispatch => {
+export const setUserQuestion = (question) => async dispatch => {
+  await AsyncStorage.setItem('question', question);
+
+  try {
+    dispatch({
+      type: SET_USER_LEVEL,
+      payload: question
+    });
+  } catch (err) {
+    console.error(err.response);
+  }
+};
+
+export const setUserLevel = (level) => async dispatch => {
+  await AsyncStorage.setItem('level', level);
+
   try {
     dispatch({
       type: SET_USER_LEVEL,
@@ -16,8 +32,10 @@ export const setUserLevel = (level) => dispatch => {
   }
 };
 
-export const setUserDiffculty = (difficulty) => dispatch => {
+export const setUserDiffculty = (difficulty) => async dispatch => {
   try {
+    await AsyncStorage.setItem('difficulty', difficulty);
+
     dispatch({
       type: SET_USER_DIFFCULTY,
       payload: difficulty
@@ -27,8 +45,10 @@ export const setUserDiffculty = (difficulty) => dispatch => {
   }
 };
 
-export const setUserScore = (score) => dispatch => {
+export const setUserScore = (score) => async dispatch => {
   try {
+    await AsyncStorage.setItem('score', score);
+
     dispatch({
       type: SET_USER_SCORE,
       payload: score
@@ -41,11 +61,14 @@ export const setUserScore = (score) => dispatch => {
 export const setUserData = () => {
   return async dispatch => {
     try {
+      const question = await AsyncStorage.getItem('question');
       const level = await AsyncStorage.getItem('level');
       const score = await AsyncStorage.getItem('score');
       const difficulty = await AsyncStorage.getItem('difficulty');
 
-      if (level === null || score === null || difficulty == null) {
+      if (level === null || score === null
+        || difficulty == null || question == null) {
+        await AsyncStorage.setItem('question', '0');
         await AsyncStorage.setItem('level', '0');
         await AsyncStorage.setItem('score', '0');
         await AsyncStorage.setItem('difficulty', 'easy');
@@ -54,6 +77,7 @@ export const setUserData = () => {
       dispatch({
         type: SET_USER_DATA,
         payload: {
+          question: question,
           level: level,
           score: score,
           difficulty: difficulty
--- client/sources/reducers/userReducer.js
+++ client/sources/reducers/userReducer.js
@@ -1,20 +1,26 @@
 import {
+  SET_USER_QUESTION,
   SET_USER_LEVEL,
   SET_USER_DIFFCULTY,
   SET_USER_SCORE,
-  SET_USER_DATA
+  SET_USER_DATA,
 } from './Actions/User';
 
 const initState = {
   user : {
+    question : 0,
     level: 0,
     difficulty: 'easy',
-    score: 0
+    score: 0,
   }
 };
 
 function quizReducer(state = initState, action) {
   switch (action.type) {
+  case SET_USER_QUESTION:
+    return {...state, user: {
+      ...state, question: action.payload
+    }};
   case SET_USER_LEVEL:
     return {...state, user: {
       ...state, level: action.payload
