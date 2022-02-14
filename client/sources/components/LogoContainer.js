7eecf581cb27dff3545ffcec12490995ee1a5201
Mehdi <mehdi.zehri@epitech.eu>
2022-02-14 02:39:01 +0100

    ✨ Add level display handling.

--- client/sources/components/LogoContainer.js
+++ client/sources/components/LogoContainer.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
 import { Headline } from 'react-native-paper';
+import PropTypes from 'prop-types';
 
-const LogoContainer = () => {
+const getLevel = (difficulty) => {
+  switch (difficulty) {
+  case 1:
+    return 'EASY';
+  case 2:
+    return 'MEDIUM';
+  case 3:
+    return 'HARD';
+  default:
+    break;
+  }
+};
+
+const LogoContainer = ({difficulty}) => {
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <Image source={require('../assets/logo.png')} style={styles.logo}/>
       </View>
       <View style={styles.titleContainer}>
-        <Headline style={styles.title}>MEDUIM</Headline>
-        <Headline style={styles.level}>LEVEL 30</Headline>
+        <Headline style={styles.title}>{getLevel(difficulty)}</Headline>
+        <Headline style={styles.level}>LEVEL {difficulty}</Headline>
       </View>
     </SafeAreaView>
   );
@@ -41,5 +55,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
   }
 });
+
+LogoContainer.propTypes = {
+  difficulty: PropTypes.number.isRequired,
+};
+
 
 export default LogoContainer;
